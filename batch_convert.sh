e5d4ce1d401d74c5be6cd41c9f6f96e32e5ce7bc
Sam Hedin <sam.hedin@gmail.com>
2020-07-21 00:22:07 +0200

    Add missing slash in batch_convert.sh

--- batch_convert.sh
+++ batch_convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -lt 2 ]; then
-    echo "You need to provide both input and output dir"
+    echo "You need to provide both input and output dir: sh batch_convert <indir> <outdir>"
     exit 1
 fi
 
@@ -8,6 +8,6 @@ mkdir -p $2
 for f in $(find $1 -name '*.html')
 do
     if [[ -f "$f" ]]; then
-        pandoc "$f" --filter rustdoc-to-org-exe -o "$2$(basename "${f%.*}").org"
+        pandoc "$f" --filter rustdoc-to-org-exe -o "$2/$(basename "${f%.*}").org"
     fi
 done
