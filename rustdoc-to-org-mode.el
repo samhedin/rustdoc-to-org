73a5bd54838e892e52435a5311a6e1e54c7b1308
Sam Hedin <sam.hedin@gmail.com>
2020-07-24 18:22:33 +0200

    comments and remove sleep

--- rustdoc-to-org-mode.el
+++ rustdoc-to-org-mode.el
@@ -9,85 +9,93 @@
   "Search the rust documentation for SEARCH-TERM.
 Only searches in headers (structs, functions, traits, enums, etc) to limit the number of results.
 Provide `prefix-arg` to only search for Level 1 headers to limit the number of search results even further. This is useful if you want to search for the name of a struct, enum or trait."
-  (interactive
-   (list
-    (read-string (format "search term, default (%s): " (thing-at-point 'symbol))
-                 nil nil (thing-at-point 'symbol))))
-
+  (interactive (list (read-string (format "search term, default (%s): "
+                                          (thing-at-point 'symbol))
+                                  nil
+                                  nil
+                                  (thing-at-point 'symbol))))
   (let ((helm-ag-base-command "rg  --smart-case --no-heading --color=never --line-number")
         (regex (if current-prefix-arg
                    (progn
                      (setq current-prefix-arg nil)
                      "^\\* [^-]\*")
                  "\\* [^-]\*")))
-
-    (helm-ag rustdoc-to-org-search-directory (concat regex search-term))))
+    (helm-ag rustdoc-to-org-search-directory
+             (concat regex search-term))))
 
 ;;;###autoload
 (defun rustdoc-to-org--install-binary ()
   "Install or update the rustdoc-to-org filter"
   (interactive)
   (let ((default-directory "~/.local/bin"))
-    (url-copy-file "https://github.com/samhedin/rustdoc-to-org/releases/download/v0.3/rustdoc-to-org-exe" "rustdoc-to-org-exe")
-    (start-process "make_executable" nil "chmod" "+x" "rustdoc-to-org-exe")))
+    (url-copy-file "https://github.com/samhedin/rustdoc-to-org/releases/download/v0.3/rustdoc-to-org-exe"
+                   "rustdoc-to-org-exe")
+    (start-process "make_executable" nil "chmod"
+                   "+x" "rustdoc-to-org-exe")))
 
 ;;;###autoload
 (defun rustdoc-to-org--convert-directory (&optional directory)
   "Convert all .html files in `DIRECTORY' and its subdirectories to org and place the files in `rustdoc-to-org-search-directory`
 If `DIRECTORY` is not given, prompts user to select directory."
   (interactive)
-
   (when (not (file-directory-p rustdoc-to-org-search-directory))
     (make-directory rustdoc-to-org-search-directory))
-
   (let ((default-directory "~/.local/bin/")
         (dir (if directory
                  directory
                (read-directory-name "Directory with rust html docs (for std: ~/.rustup/toolchains/<dir>/share/doc/rust/html/std): "))))
+    (when (not (file-exists-p "rustdoc-to-org-exe"))
+      (if (string= "yes"
+                   (read-string "Could not find rustdoc-to-org pandoc filter in your path, would you like to install it? "
+                                "yes"))
+          (rustdoc-to-org--install-binary)
+        (message "could not find pandoc filter, this will not work!")))
+    (message "Batch converting files, this might take a while!")
 
-   (when (not (file-exists-p "rustdoc-to-org-exe"))
-     (if (string= "yes" (read-string "Could not find rustdoc-to-org pandoc filter in your path, would you like to install it? " "yes"))
-         (rustdoc-to-org--install-binary)
-       (message "could not find pandoc filter, this will not work!")))
-
-   (message "Batch converting files, this might take a while!")
-
-  (dolist (file (directory-files-recursively dir ".html"))
-    (when (< 10 (count-lines (point-min) (point-max)))
-      (progn
-        (sleep-for 0.05)
-        (convert-file dir file))))
-  (message "Almost done!")))
+    (dolist (file (directory-files-recursively dir ".html"))
+      (when (with-temp-buffer
+              (insert-file-contents file)
+              (< 10 (count-lines (point-min)
+                                 (point-max))))
+        (progn
+          (convert-file dir file))))
+    (message "Done!")))
 
 (defun convert-file (dir file)
-  (let* ((outputfile (file-truename (concat rustdoc-to-org-search-directory "/" (file-name-sans-extension (file-relative-name file dir)) ".org")))
-
+  (let* ((outputfile  (concat rustdoc-to-org-search-directory
+                              "/"
+                              (file-name-sans-extension (file-relative-name file dir))
+                              ".org"))
          ;; Save the outputfilename in a closure that will be called when the conversion is finished
          (callback (lambda (p e)
-                     (message "converting %s " file)
                      (remove-whitespace outputfile)))
-
          (process (progn
-                    (make-directory (file-name-directory outputfile) t)
-                    (start-process "convert" "*pandoc-log*" "pandoc"
+                    (make-directory (file-name-directory outputfile)
+                                    t)
+                    (start-process "convert"
+                                   "*pandoc-log*"
+                                   "pandoc"
                                    (shell-quote-argument file)
-                                   "--filter"  "rustdoc-to-org-exe"
-                                   "-o" (shell-quote-argument outputfile)))))
-
+                                   "--filter"
+                                   "rustdoc-to-org-exe"
+                                   "-o"
+                                   (shell-quote-argument outputfile)))))
     (set-process-sentinel process callback)))
+
 
 ;;;###autoload
 (defun remove-whitespace (outputfile)
   (condition-case nil
-  (with-temp-file outputfile
-    (insert-file-contents outputfile)
-
-    (goto-char (point-min))
-    (while (not (eobp))
-      (when (and (current-line-empty-p) (not (next-line-is-header-p))) ;;Delete all whitespace, unless the next line is a header.
-        (kill-whole-line))
-      (forward-line)))
-  (error (message "Could not remove whitespace from %s " outputfile))))
+      (with-temp-file outputfile
+        (insert-file-contents outputfile)
+        (goto-char (point-min))
+        (while (not (eobp))
+          (when (and (current-line-empty-p)
+                     (not (next-line-is-header-p))) ;;Delete all whitespace, unless the next line is a header.
+            (kill-whole-line))
+          (forward-line)))
+    (error (message "Could not remove whitespace from %s "
+                    outputfile))))
 
 ;;;###autoload
 (defun current-line-empty-p ()
