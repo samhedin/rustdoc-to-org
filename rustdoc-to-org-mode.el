7caabc8a0c72341146d1adef6d600f946bf1f6ac
Sam Hedin <sam.hedin@gmail.com>
2020-07-24 08:09:47 +0200

    Add ability to update filter

--- rustdoc-to-org-mode.el
+++ rustdoc-to-org-mode.el
@@ -25,9 +25,10 @@ Provide `prefix-arg` to only search for Level 1 headers to limit the number of s
 
 ;;;###autoload
 (defun rustdoc-to-org--install-binary ()
-  "Install the rustdoc-to-org filter"
+  "Install or update the rustdoc-to-org filter"
+  (interactive)
   (let ((default-directory "~/.local/bin"))
-    (url-copy-file "https://github.com/samhedin/rustdoc-to-org/releases/download/v0.2/rustdoc-to-org-exe" "rustdoc-to-org-exe")
+    (url-copy-file "https://github.com/samhedin/rustdoc-to-org/releases/download/v0.2/rustdoc-to-org-exe" "rustdoc-to-org-exe" 'replace)
     (start-process "make_executable" nil "chmod" "+x" "rustdoc-to-org-exe")))
 
 ;;;###autoload
