57442edde849d2c429d47f47adeaaefa34daf37e
Sam Hedin <sam.hedin@gmail.com>
2020-07-25 19:07:11 +0200

    linebreaks and links

--- debug_files/filter.lua
+++ debug_files/filter.lua
@@ -15,6 +15,20 @@ function map(tbl, f)
   return t
 end
 
+Space = pandoc.Str(" ")
+LineBreak = pandoc.Str(" ")
+SoftBreak = pandoc.Str(" ")
+
+Str = function(el)
+  if el.text == "[src]" then
+    return pandoc.Str(" ")
+  end
+end
+
+Link = function(el)
+  return pandoc.Span(el.content)
+end
+
 
 Div = function(el)
   if elem(el.classes, "shortcuts") or elem(el.classes, "sidebar-elems") or elem(el.classes, "theme-picker") or elem(el.classes, "infos") or elem(el.classes, "search-container")  or elem(el.classes,"sidebar-menu") or elem(el.classes, "logo-container") or elem(el.classes, "toggle-wrapper") then
@@ -22,14 +36,10 @@ Div = function(el)
   end
 end
 
-function CodeBlock(el)
+CodeBlock = function(el)
   if elem(el.classes, "line-numbers") then
     return pandoc.Null
   else
     return pandoc.Para(pandoc.Str("#+BEGIN_SRC rust \n" .. el.text .. "\n#+END_SRC"))
   end
-end
-
-Header = function(el)
-  return pandoc.Header(5, el.content)
 end
