026d17639363b58eb83447788fbaa4bab60cb37b
Sam Hedin <sam.hedin@gmail.com>
2020-07-25 19:32:55 +0200

    Try using pandoc walk

--- debug_files/filter.lua
+++ debug_files/filter.lua
@@ -15,26 +15,27 @@ function map(tbl, f)
   return t
 end
 
-Space = pandoc.Str(" ")
-LineBreak = pandoc.Str(" ")
-SoftBreak = pandoc.Str(" ")
+filters = {
+Space = pandoc.Str(" "),
+LineBreak = pandoc.Str(" "),
+SoftBreak = pandoc.Str(" "),
 
 Str = function(el)
   if el.text == "[src]" then
     return pandoc.Str(" ")
   end
-end
+end,
 
 Link = function(el)
   return pandoc.Span(el.content)
-end
+end,
 
 
 Div = function(el)
   if elem(el.classes, "shortcuts") or elem(el.classes, "sidebar-elems") or elem(el.classes, "theme-picker") or elem(el.classes, "infos") or elem(el.classes, "search-container")  or elem(el.classes,"sidebar-menu") or elem(el.classes, "logo-container") or elem(el.classes, "toggle-wrapper") then
     return pandoc.Null
   end
-end
+end,
 
 CodeBlock = function(el)
   if elem(el.classes, "line-numbers") then
@@ -42,4 +43,9 @@ CodeBlock = function(el)
   else
     return pandoc.Para(pandoc.Str("#+BEGIN_SRC rust \n" .. el.text .. "\n#+END_SRC"))
   end
+end
+}
+
+function Pandoc(el)
+  return pandoc.Pandoc(pandoc.walk_block(pandoc.Div(el.blocks), filters), pandoc.Meta({}))
 end
