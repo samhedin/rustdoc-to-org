8b74bdb1f9c8b983352318c6a2b502ae005daaee
Sam Hedin <sam.hedin@gmail.com>
2020-07-25 18:25:08 +0200

    CodeBlocks with syntax highlighting

--- debug_files/filter.lua
+++ debug_files/filter.lua
@@ -7,11 +7,27 @@ local function elem(tab, val)
   return false
 end
 
+function map(tbl, f)
+  local t = {}
+  for k,v in pairs(tbl) do
+    t[k] = f(v)
+  end
+  return t
+end
+
 Div = function(el)
-  if elem(el.classes, "shortcuts") or elem(el.classes, "sidebar-elems") or elem(el.classes, "theme-picker") or elem(el.classes, "infos") or elem(el.classes, "search-container")  or elem(el.classes,"sidebar-menu")   or elem(el.classes, "logo-container")  or elem(el.classes, "toggle-wrapper") then
+  if elem(el.classes, "shortcuts") or elem(el.classes, "sidebar-elems") or elem(el.classes, "theme-picker") or elem(el.classes, "infos") or elem(el.classes, "search-container")  or elem(el.classes,"sidebar-menu") or elem(el.classes, "logo-container") or elem(el.classes, "toggle-wrapper") then
+    return pandoc.Null
+      else
+    return el
+  end
+end
+
+CodeBlock = function(el)
+  if elem(el.classes, "line-numbers") then
     return pandoc.Null
   else
-    return el
+    return pandoc.Para(pandoc.Str("#+BEGIN_SRC rust \n" .. el.text .. "\n#+END_SRC"))
   end
 end
 
