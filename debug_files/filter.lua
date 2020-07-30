a07b6addfd3a1e1b628ba891f990bff17cf03ad7
Sam Hedin <sam.hedin@gmail.com>
2020-07-30 07:31:05 +0200

    elem -> includes

--- debug_files/filter.lua
+++ debug_files/filter.lua
@@ -1,12 +1,3 @@
-local function elem(tab, val)
-  for index, value in ipairs(tab) do
-    if value == val then
-      return true
-    end
-  end
-  return false
-end
-
 function map(tbl, f)
   local t = {}
   for k,v in pairs(tbl) do
@@ -32,18 +23,19 @@ end,
 
 
 Div = function(el)
-  if elem(el.classes, "shortcuts") or elem(el.classes, "sidebar-elems") or elem(el.classes, "theme-picker") or elem(el.classes, "infos") or elem(el.classes, "search-container")  or elem(el.classes,"sidebar-menu") or elem(el.classes, "logo-container") or elem(el.classes, "toggle-wrapper") then
+  if el.classes:includes("shortcuts") or el.classes:includes("sidebar-elems") or el.classes:includes("theme-picker") or el.classes:includes("infos") or el.classes:includes("search-container")  or el.classes:includes("sidebar-menu") or el.classes:includes("logo-container") or el.classes:includes("toggle-wrapper") then
     return pandoc.Null
   end
 end,
 
 CodeBlock = function(el)
-  if elem(el.classes, "line-numbers") then
+  if el.classes:includes("line-numbers") then
     return pandoc.Null
   else
     return pandoc.Para(pandoc.Str("#+BEGIN_SRC rust \n" .. el.text .. "\n#+END_SRC"))
   end
-end
+end,
+
 }
 
 function Pandoc(el)
