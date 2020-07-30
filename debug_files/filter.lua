8114b69dd878b164940dcd241f0788bad95d916f
Sam Hedin <sam.hedin@gmail.com>
2020-07-30 13:31:40 +0200

    filter example header

--- debug_files/filter.lua
+++ debug_files/filter.lua
@@ -1,32 +1,53 @@
-function map(tbl, f)
-  local t = {}
-  for k,v in pairs(tbl) do
-    t[k] = f(v)
+local function cleaninlines(content)
+  for i,v in ipairs(content) do
+    content[i] = pandoc.walk_inline(content[i], cleaninline)
   end
-  return t
+  return content
 end
 
-filters = {
-Space = pandoc.Str(" "),
-LineBreak = pandoc.Str(" "),
-SoftBreak = pandoc.Str(" "),
-
-Str = function(el)
-  if el.text == "[src]" then
+cleaninline = {
+  Space = function(el)
     return pandoc.Str(" ")
-  end
-end,
+  end,
+  LineBreak = function(el)
+    return pandoc.Str(" ")
+  end,
+  SoftBreak = function(el)
+    return pandoc.Str(" ")
+    end,
+}
 
+cleanblocks = {
+  Str = function(el)
+    if el.text == "[src]" then
+      return pandoc.Str(" ")
+    end
+  end,
 Link = function(el)
   return pandoc.Span(el.content)
 end,
 
+Header = function(el)
+  if el.classes:includes("section-header") then
+    return pandoc.Null
+  end
+end,
 
 Div = function(el)
   if el.classes:includes("shortcuts") or el.classes:includes("sidebar-elems") or el.classes:includes("theme-picker") or el.classes:includes("infos") or el.classes:includes("search-container")  or el.classes:includes("sidebar-menu") or el.classes:includes("logo-container") or el.classes:includes("toggle-wrapper") then
     return pandoc.Null
   end
 end,
+
+Plain = function(el)
+  return pandoc.Plain(cleaninlines(el.content))
+end,
+
+
+Para = function(el)
+  return pandoc.Para(cleaninlines(el.content))
+end,
+
 
 CodeBlock = function(el)
   if el.classes:includes("line-numbers") then
@@ -39,5 +60,5 @@ end,
 }
 
 function Pandoc(el)
-  return pandoc.Pandoc(pandoc.walk_block(pandoc.Div(el.blocks), filters), pandoc.Meta({}))
+  return pandoc.Pandoc(pandoc.walk_block(pandoc.Div(el.blocks), cleanblocks), pandoc.Meta({}))
 end
