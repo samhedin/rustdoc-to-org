7ff95cbdcacf5534fccae4a0208d653e87230b29
Jonas Møller <jonas.moller@student.uib.no>
2020-08-03 17:48:15 +0200

    Removed rustup show hack in favour of doc --path

--- convert.sh
+++ convert.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-rustup doc
-
 LUA_FILTER=filter.lua
-
-if [ "$RUSTUP_HOME" = "" ]; then
-   RUSTUP_HOME="$HOME/.rustup"
-fi
 
 function num_cpus {
     rg -c '^$' /proc/cpuinfo
 }
 
-function get_toolchain {
-    rustup show | sed -nr 's/(.*) \(default\)/\1/p' | head -n 1
-}
+DOC_PATH="$(dirname "$(rustup doc --std --path)")"
 
 fd . \
     -ehtml \
-    "$RUSTUP_HOME/toolchains/$(get_toolchain)/share/doc/rust/html/std" \
+    "$DOC_PATH" \
     -j$(num_cpus) \
     -x pandoc '{}' \
     --lua-filter "$LUA_FILTER" \
