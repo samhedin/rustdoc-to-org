00e8ea337c9571068edae61f989b4fe6d8d9cafb
Sam Hedin <sam.hedin@gmail.com>
2020-08-16 18:08:32 +0200

    Update regex to only match on lines starting with *

--- rustdoc.el
+++ rustdoc.el
@@ -126,7 +126,7 @@ Level 1 headers are things like struct or enum names."
                     (progn
                       (setq current-prefix-arg nil) ; If this is not done, helm-ag will pick up the prefix arg too and do funny business.
                       "^\\* [^-(<]*")
-                  "\\* [^-(<]*"))
+                  "^\\*+ [^-(<]*"))
         (regexed-search-term (concat regex (seq-reduce (lambda (acc s)
                                                     (concat acc "[^-(<]*" s)) (split-string search-term " ") "")))) ; This turns a search for `enum option' into `enum.*option', which lets there be chars between the terms
     (update-current-project)
